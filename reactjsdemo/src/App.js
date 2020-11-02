91de430bd2a9c06652d3cdceae339ecd147eb9f8
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-11-02 02:22:17 +0000

    react events

--- reactjsdemo/src/App.js
+++ reactjsdemo/src/App.js
@@ -1,18 +1,34 @@
+import React, { Component }  from 'react';
 import logo from './logo.svg';
 import './App.css';
-import './TaskList';
 import TaskList from './TaskList';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>To-Do List</h1>
-        <input></input>
-        <button>Add To-Do</button>
-        <TaskList />
-        <TaskList />
-    </div>
-  );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            todoList: 'item2'
+        }
+    }
+    render(){
+        return (
+            <div className="App">
+                <h1>To-do List</h1>
+                <input></input>
+                <button onClick={() =>this.addTodo()}>Add Todo</button>
+                <TaskList title={'Pending Todo'} buttonText={'Done'} tasks={this.state.todoList} buttonFunction={this.removeTodo()} />
+                <TaskList title={'Completed'} buttonText={"Delete"} tasks={'done'} buttonFunction={this.removeTodo()} />
+            </div>
+        );
+    }
+    addTodo(){
+        this.setState({ todoList: 'item1'});
+    }
+    removeTodo(){
+        if(this.state.todoList != null){
+            this.setState({ todoList: null})
+        }
+    }
 }
 
 export default App;
